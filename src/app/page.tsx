b5cc1cc192bdacb04ebcb6a492292b5e1cf49404
mamun <mamun.orrashid@brainstation-23.com>
2025-04-19 16:48:04 +0600

    Update page.tsx with latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import MainContent from '@/components/MainContent';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Fametonic - Grow Your Social Media Presence',
+  description: 'Boost your social media presence with personalized insights and strategies.',
+};
+
 export default function Home() {
   return (
       <MainContent/>
